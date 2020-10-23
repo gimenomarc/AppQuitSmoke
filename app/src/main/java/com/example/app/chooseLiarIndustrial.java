d001404809e3388e45c2fa536dfb4c2bd7896035
SirCrewete <gimenocrew1996@gmail.com>
2020-10-23 18:28:48 +0200

    Nuevo Activity para escoger tabaco

--- app/src/main/java/com/example/app/chooseLiarIndustrial.java
+++ app/src/main/java/com/example/app/chooseLiarIndustrial.java
@@ -2,7 +2,9 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class chooseLiarIndustrial extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class chooseLiarIndustrial extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_liar_industrial);
     }
+
+    public void intentLogWithLiar (View v){
+        Intent intentLogeadoWithLiar = new Intent (this, insideApp.class);
+        startActivity(intentLogeadoWithLiar);
+    }
+
+    public void intentLogWithIndu (View v){
+        Intent intentLogeadoWithIndu = new Intent (this, insideApp.class);
+        startActivity(intentLogeadoWithIndu);
+    }
+
 }
